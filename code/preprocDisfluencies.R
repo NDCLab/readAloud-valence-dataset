96331618eb47674de577a5a5dc00c0a6430a3ad4
JMA <77893711+jessb0t@users.noreply.github.com>
2022-04-19 10:06:54 -0500

    Minor updates

--- code/preprocDisfluencies.R
+++ code/preprocDisfluencies.R
@@ -23,12 +23,15 @@ out_path <- '/Users/jalexand/github/readAloud-valence-dataset/derivatives/prepro
 sub_folders <- list.files(input_path, pattern = "sub")
 
 #create dataframe for storing output data and define output file name
-disfluencySummaryDat <- data.frame(matrix(ncol=5, nrow=0))
+disfluencySummaryDat <- data.frame(matrix(ncol=8, nrow=0))
 colnames(disfluencySummaryDat) <- c("id",
                                     "passage",
                                     "percDisfluent_firstHalf",
                                     "percDisfluent_lastHalf",
-                                    "percDisfluent_switch")
+                                    "percDisfluent_prePREswitch",
+                                    "percDisfluent_preswitch",
+                                    "percDisfluent_switch",
+                                    "percDisfluent_postswitch")
 disfluency_out <- paste("disfluencies_subject-by-passage_", today, ".csv", sep="", collapse=NULL)
 
 ### SECTION 2: START PARTICIPANT LOOP
@@ -59,7 +62,7 @@ for(i in 1:length(sub_folders)){
     #add column to indicate all disfluent syllables
     passageErrors$disfluent <- rowSums(passageErrors[,6:14])>0
     
-    #calculate percentage disfluency in each passage half and switch group
+    #calculate percentage disfluency in each passage half
     #first half
     passageErrors_first <- passageErrors[1:(match("switch", passageErrors$wordGroup)-1),]
     numSyll_first <- length(passageErrors_first$disfluent) #count number of syllables in group
@@ -74,6 +77,21 @@ for(i in 1:length(sub_folders)){
     numSyll_last_disfluent <- length(passageErrors_last_disfluent$disfluent) #count number of disfluent syllables
     percDisfluent_lastHalf <- numSyll_last_disfluent / numSyll_last
     
+    #calculate percentage disfluency in word groups around the switch
+    #prePREswitch group
+    passageErrors_prePREswitch <- passageErrors[passageErrors$wordGroup %in% c("prePREswitchGroup"),] #subset to prePREswitch group rows
+    numSyll_prePREswitch <- length(passageErrors_prePREswitch$disfluent) #count number of syllables in group
+    passageErrors_prePREswitch_disfluent <- passageErrors_prePREswitch[passageErrors_prePREswitch$disfluent,] #subset to disfluent rows
+    numSyll_prePREswitch_disfluent <- length(passageErrors_prePREswitch_disfluent$disfluent) #count number of disfluent syllables
+    percDisfluent_prePREswitch <- numSyll_prePREswitch_disfluent / numSyll_prePREswitch
+    
+    #preswitch group
+    passageErrors_preswitch <- passageErrors[passageErrors$wordGroup %in% c("preswitchGroup"),] #subset to preswitch group rows
+    numSyll_preswitch <- length(passageErrors_preswitch$disfluent) #count number of syllables in group
+    passageErrors_preswitch_disfluent <- passageErrors_preswitch[passageErrors_preswitch$disfluent,] #subset to disfluent rows
+    numSyll_preswitch_disfluent <- length(passageErrors_preswitch_disfluent$disfluent) #count number of disfluent syllables
+    percDisfluent_preswitch <- numSyll_preswitch_disfluent / numSyll_preswitch
+    
     #switch group
     passageErrors_switch <- passageErrors[passageErrors$wordGroup %in% c("switchGroup", "switch"),] #subset to switch group rows
     numSyll_switch <- length(passageErrors_switch$disfluent) #count number of syllables in group
@@ -81,13 +99,23 @@ for(i in 1:length(sub_folders)){
     numSyll_switch_disfluent <- length(passageErrors_switch_disfluent$disfluent) #count number of disfluent syllables
     percDisfluent_switch <- numSyll_switch_disfluent / numSyll_switch
     
+    #postswitch group
+    passageErrors_postswitch <- passageErrors[passageErrors$wordGroup %in% c("postswitchGroup"),] #subset to postswitch group rows
+    numSyll_postswitch <- length(passageErrors_postswitch$disfluent) #count number of syllables in group
+    passageErrors_postswitch_disfluent <- passageErrors_postswitch[passageErrors_postswitch$disfluent,] #subset to disfluent rows
+    numSyll_postswitch_disfluent <- length(passageErrors_postswitch_disfluent$disfluent) #count number of disfluent syllables
+    percDisfluent_postswitch <- numSyll_postswitch_disfluent / numSyll_postswitch
+    
     
     #store output data in summary matrices
     disfluencySummaryDat[nrow(disfluencySummaryDat) + 1,] <-c(id,
                                                               passage,
                                                               percDisfluent_firstHalf,
                                                               percDisfluent_lastHalf,
-                                                              percDisfluent_switch)
+                                                              percDisfluent_prePREswitch,
+                                                              percDisfluent_preswitch,
+                                                              percDisfluent_switch,
+                                                              percDisfluent_postswitch)
   
   }
 }
