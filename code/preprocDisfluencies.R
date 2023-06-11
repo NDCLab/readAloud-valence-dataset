1a979ccac5c3aaa61deeda872ecdb4112e9bd4dd
l-acs <lucas.sahar@mail.mcgill.ca>
2023-06-11 15:40:11 -0400

    Finalize cleanup. Simplify IO logic. Remove unelucidative helpers.

--- code/preprocDisfluencies.R
+++ code/preprocDisfluencies.R
@@ -23,11 +23,6 @@ error_types_idiomatic = c(
   "filled_pause", "hesitation", "elongation", "corrected"
 )
 
-folder_regex_default = "sub-\\d{6}_reconciled"
-ext_default = 'csv'
-tz_default = "America/New_York"
-date_format_default = "%Y%m%d_%I%M%P"
-
 # if we're in debug mode, write output dataframes to disk as they are made
 incremental_writeout = if(DEBUG_MODE) "incremental-passages_debugging" else NULL
 
@@ -46,6 +41,7 @@ fill_dummy <- function(df, cols) cbind(df, setNames(lapply(cols, dummy), cols)) 
 ## Calculations about the passages themselves, for things like word ratios
 # base = "~/Documents/ndclab/analysis-sandbox/github-structure-mirror/readAloud-valence-dataset"
 base = "/home/data/NDClab/datasets/readAloud-valence-dataset"
+timestamp = now("America/New_York") %>% format("%Y%m%d_%I%M%P")
 scaffolds_path = paste(base, "code/scaffolds.xlsx", sep = '/')
 titles = excel_sheets(scaffolds_path) # antarctica ... vegas
 
@@ -112,33 +108,35 @@ complain_when_invalid <- function(passage_df, participant_id, passage_name) {
   return(passage_df)
 }
 
+errcols <- function(passage_df) select(passage_df, misprod:elongation)
+
 count_errors_by_type <- function(passage_df)
-  passage_df %>% select(misprod:elongation) %>% map_df(as.numeric) %>% colSums
+  errcols(passage_df) %>% map_df(as.numeric) %>% colSums %>% t %>% as.data.frame
 
 count_error_syllables_any_type <- function(passage_df) # grand total, across types
   passage_df %>%
-    select(misprod:elongation) %>% # only the error columns
+    errcols %>% # only the error columns
     get_rows_with_a_one %>% # only the rows that have an error somewhere
     nrow # count them
 
 count_corrected_error_syllables <- function(passage_df)
   passage_df %>% 
     filter(corrected == 1) %>% # only get corrected rows
-    select(misprod:elongation) %>% # now let's only look at their error columns
+    errcols %>% # now let's only look at their error columns
     get_rows_with_a_one %>% # only the rows that have an error somewhere
     nrow
 
 count_uncorrected_error_syllables <- function(passage_df)
   passage_df %>% 
     filter(corrected == 0) %>% # only get uncorrected ones
-    select(misprod:elongation) %>% # now let's only look at their error columns
+    errcols %>% # now let's only look at their error columns
     get_rows_with_a_one %>% # only the rows that have an error somewhere
     nrow
 
 count_distinct <- function(passage_df, passage_scaffold, error_type)
   # Distinct if they don't touch. But if they're in separate words, they can touch.
   cbind(passage_scaffold, passage_df) %>% # align scaffold info with annotated syllables
-    filter({{error_type}} == 1 & (wordOnset == 1 | lag({{error_type}}) != 1)) %>% # an error that's (a) word-initial or (b) *not* preceded by an adjacent one
+    filter({{error_type}} == 1 & (wordOnset == 1 | lag({{error_type}}) != 1)) %>% # an error that's (a) a new word or (b) *not* preceded by an adjacent error
     nrow
 
 count_distinct_across_words <- function(passage_df, passage_scaffold, error_type)
@@ -199,7 +197,7 @@ append_lookbacks_multicol <- function(df, colrange, lookback_count)
          .init = df)
 
 
-a_b_sequence_multicol <- function(df, errtypes_a, errtypes_b, prior_context = 1) {   
+a_b_sequence <- function(df, errtypes_a, errtypes_b, prior_context = 1) {
   # a: LHS errors; b: RHS errors; context: how many rows back we can look for LHS errors
 
   lhs_cols = colnames_from_range(df, {{errtypes_a}})
@@ -214,8 +212,8 @@ a_b_sequence_multicol <- function(df, errtypes_a, errtypes_b, prior_context = 1)
          if_any(rhs_cols, ~ . == 1) & if_any(matches(lookbacks_regex), ~ . == 1))
 }
 
-count_a_b_sequence_multicol <- function(df, errtypes_a, errtypes_b, prior_context = 1)
-  a_b_sequence_multicol(df, {{errtypes_a}}, {{errtypes_b}}, prior_context) %>% nrow
+count_a_b_sequences <- function(df, errtypes_a, errtypes_b, prior_context = 1)
+  a_b_sequence(df, {{errtypes_a}}, {{errtypes_b}}, prior_context) %>% nrow
 
 
 error_summary <- function(passage_df, passage_name) {
@@ -232,12 +230,12 @@ error_summary <- function(passage_df, passage_name) {
     distinct_word_stress = count_distinct(passage_df, scaffolds[[passage_name]], word_stress),
     distinct_hesitation  = count_distinct_across_words(passage_df, scaffolds[[passage_name]], hesitation),
 
-    hes_after_misprod    = count_a_b_sequence_multicol(passage_df, misprod, hesitation, prior_context = 4),
-    hes_after_err        = count_a_b_sequence_multicol(passage_df, misprod:word_stress, hesitation, prior_context = 4), # all the error types not associated with speed or fluency
-    elong_after_misprod  = count_a_b_sequence_multicol(passage_df, misprod, elongation, prior_context = 4),
-    elong_after_err      = count_a_b_sequence_multicol(passage_df, misprod:word_stress, elongation, prior_context = 4), # all the error types not associated with speed or fluency
-    hes_after_elong      = count_a_b_sequence_multicol(passage_df, elongation, hesitation),
-    elong_after_hes      = count_a_b_sequence_multicol(passage_df, hesitation, elongation), # FIXME: it would be nice to generate all these at once
+    hes_after_misprod    = count_a_b_sequences(passage_df, misprod, hesitation, prior_context = 4),
+    hes_after_err        = count_a_b_sequences(passage_df, misprod:word_stress, hesitation, prior_context = 4), # all the error types not associated with speed or fluency
+    elong_after_misprod  = count_a_b_sequences(passage_df, misprod, elongation, prior_context = 4),
+    elong_after_err      = count_a_b_sequences(passage_df, misprod:word_stress, elongation, prior_context = 4), # all the error types not associated with speed or fluency
+    hes_after_elong      = count_a_b_sequences(passage_df, elongation, hesitation),
+    elong_after_hes      = count_a_b_sequences(passage_df, hesitation, elongation), # FIXME: it would be nice to generate all these at once
 
     errors               = count_error_syllables_any_type(passage_df),
     corrections          = count_corrected_error_syllables(passage_df),
@@ -276,24 +274,18 @@ error_summary_with_metadata <- function(passage_name, participant_id, dir_root) 
 }
 
 
-# all passages for a participant
-generate_summary_for_each_passage_with_metadata <- function(dir_root, participant_id, write_to = incremental_writeout, date_format = date_format_default) {
+# All passages for a participant
+generate_summary_for_each_passage_with_metadata <- function(dir_root, participant_id, write_to = incremental_writeout) {
   df = build_participant_dirname(dir_root, participant_id) %>% dir %>% # passage name _with_ the extension
       map_df(error_summary_with_metadata, participant_id, dir_root)
 
   if (!is.null(write_to) && fs::is_dir(write_to)) { # incremental CSV: just this participant, with all their passages
-    outfile_debug = paste(write_to, "/", participant_id, "_", now() %>% format(date_format), '.csv', sep = "")
+    outfile_debug = paste(write_to, "/", participant_id, "_", timestamp, '.csv', sep = "")
     write_csv(df, outfile_debug)
   }
 
   return(df)
 }
-
-# Now, for each participant under a directory, each identified by the form sub_XXXXXX_reconciled,
-# call generate_summary_for_each_passage_with_metadata(the_parentdir_of_all_those, that_id)
-
-find_participant_id_from_dirname <- function(dirname) # "sub-150077/sub-150077_reconciled" -> "150077"
-  str_extract(dirname, "\\d+")
 
 summarize_numeric_cols <- function(df, f, label = as.character(substitute(f)))
   cbind(id = label, reframe(df, across(where(is.numeric), f)))
@@ -301,40 +293,29 @@ summarize_numeric_cols <- function(df, f, label = as.character(substitute(f)))
 append_summary_stats <- function(df)
   bind_rows(df, summarize_numeric_cols(df, mean), summarize_numeric_cols(df, sd))
 
+# Now, for each participant under a directory, each identified by the form sub_XXXXXX_reconciled,
+# call generate_summary_for_each_passage_with_metadata(the_parentdir_of_all_those, that_id)
 summarize_errors_in_subdirectories <- function(dir_root, subfolder_match)
   dir_root %>%
     dir(include.dirs = TRUE, recursive = TRUE, pattern = subfolder_match) %>% # walk the directory
-    map(find_participant_id_from_dirname) %>% # split it up: sub-150079_reconciled -> 150079
+    map(\(dir) str_extract(dir,  "\\d+")) %>% # split them up: sub-150079_reconciled -> 150079
     map_df(generate_summary_for_each_passage_with_metadata, dir_root = dir_root) %>% # summarize all spreadsheets for that participant, for each participant
     append_summary_stats # add rows for mean and standard deviation
 
 # we've matched subfolders by explicitly returning directories (include.dirs = 
 # TRUE) and recursing (recursive = TRUE), thus catching -"_reconciled" subfolders
 
-# Now finally: write all our results to a file (a CSV)
-
-build_output_filename <- function(label, ext = ext_default, timezone = tz_default, date_format = date_format_default) {
-  # `label` may include the destination directory, if different from the working directory when the script is run  
-  current_datetime <- now(timezone) %>% format(date_format) # e.g. 20230520_1240pm
-
-  paste(
-    label, '_', current_datetime,
-    '.', ext,
-    sep = ""
-  )
-}
-
-compute_summary_and_write_to_file <- function(dir_root, label, ext = ext_default, timezone = tz_default, 
-                                              date_format = date_format_default, subfolder_match = folder_regex_default)
-{
-  outpath_name <- build_output_filename(label, ext, timezone, date_format)
-  summary_df <- summarize_errors_in_subdirectories(dir_root, subfolder_match)
-  
-  write_csv(summary_df, outpath_name)
-  return(outpath_name)
-}
-
+## Now finally: write all our results to a file (a CSV)
 annotations_base = paste(base, "derivatives/preprocessed", sep = '/')
 github_root = paste(annotations_base, "error-coding", sep = '/')
-outname_base = paste(annotations_base, "disfluencies_subject-x-passage", sep = '/')
-compute_summary_and_write_to_file(github_root, outname_base)
+
+label = "disfluencies_subject-x-passage_"
+
+outpath <- paste(sep = "", annotations_base, '/', label, timestamp, ".csv")
+# e.g. "./some/path/disfluencies_20230520_1240pm.csv"
+
+github_root %>%
+  summarize_errors_in_subdirectories("sub-\\d{6}_reconciled") %>%
+  write_csv(outpath)
+
+print(outpath)
